843318308afcf02707260e4b22b08584ff4d38f5
Yuri Cavalcante <yurilimacavalcante@gmail.com>
2017-08-28 10:03:13 -0300

    Criado componente Account

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-account',
+    template: `
+        <div class="row">
+            <div class="col-xs-12">
+                Minha conta
+            </div>
+        </div>
+    `
+})
+export class AccountComponent {
+
+}
