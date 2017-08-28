a6638acb073b59f4c82f819b519f93aefeffd4d2
Yuri Cavalcante <yurilimacavalcante@gmail.com>
2017-08-28 10:43:25 -0300

    criado template inicial da conta

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-account',
-    template: `
-        <div class="row">
-            <div class="col-xs-12">
-                Minha conta
-            </div>
-        </div>
-    `
+    templateUrl: './account.component.html'
 })
 export class AccountComponent {
 
