4edc9e3ad2b5d99e1a6f8ba2a68a8e3351926d9b
Yuri Cavalcante <yurilimacavalcante@gmail.com>
2017-08-29 17:37:34 -0300

    implementado leitura de parametros da rota

--- src/app/account/account-edit/account-edit.component.ts
+++ src/app/account/account-edit/account-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnChanges, OnInit, Input, Output, EventEmitter,
           ViewChild, ElementRef, DoCheck,
           AfterContentInit, OnDestroy, Renderer2 } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
 import { Account } from '../account.model';
 import { AccountsService } from "../accounts.service";
 
@@ -10,12 +12,12 @@ import { AccountsService } from "../accounts.service";
   styleUrls: ['./account-edit.component.css']
 })
 export class AccountEditComponent implements 
-  OnChanges, OnInit, DoCheck,
-  AfterContentInit, OnDestroy {
+  OnInit, OnDestroy {
 
   conta: Account;
 
-  @Input('index') index: number;
+  //@Input('index')
+  index: number;
 
   //@Output() accountSaved = new EventEmitter<Account>();
 
@@ -23,42 +25,46 @@ export class AccountEditComponent implements
   @ViewChild('saldo') saldoFormGroup: ElementRef;
 
   constructor(private renderer: Renderer2,
-              private accountsService: AccountsService) { }
+              private accountsService: AccountsService,
+              private route: ActivatedRoute) { }
 
-  ngOnChanges() {
-    if(this.index != null) {
-      const conta = this.accountsService.getAccount(this.index);
-      this.conta = new Account(
-        conta.agencia,
-        conta.conta,
-        conta.tipo,
-        conta.saldo
-      );
+  ngOnInit() {
+    // sincrono
+    this.index = this.route.snapshot.params['id'];
+    this.loadAccount();
+
+    // assyncrono
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.index = params['id'];
+        this.loadAccount();
+      }
+    );
+    // console.log(this.renderer);
+    // if(this.conta.saldo < 0) {
+    //   this.renderer.addClass(
+    //     this.saldoFormGroup.nativeElement,
+    //     'has-error'
+    //   );
+    // } else {
+    //   this.renderer.removeClass(
+    //     this.saldoFormGroup.nativeElement,
+    //     'has-error'
+    //   );
+    // }
+      //this.saldoFormGroup.nativeElement.className = 'form-group has-error';
+  }
+
+  loadAccount() {
+    if(this.index) {
+      this.conta = this.accountsService.getAccount(this.index);
     } else {
       this.conta = new Account(0, 0, '', 0);
     }
-    console.log(this.renderer);
-    if(this.conta.saldo < 0) {
-      this.renderer.addClass(
-        this.saldoFormGroup.nativeElement,
-        'has-error'
-      );
-    } else {
-      this.renderer.removeClass(
-        this.saldoFormGroup.nativeElement,
-        'has-error'
-      );
-    }
-      //this.saldoFormGroup.nativeElement.className = 'form-group has-error';
   }
 
   log() {
     console.log(this.conta.saldo);
-  }
-
-  ngOnInit() {
-    //this.agenciaFormGroup.nativeElement.className = 'form-group has-error';
-    console.log('OnInit');
   }
 
   ngDoCheck() {
